9ea3997f8d133a2c4b396a80948e8af9ed652616
Aman Kumar <aman01197kumar@gmail.com>
2023-02-23 13:49:42 +0530

    final commit

--- src/layout/Header/Header.js
+++ src/layout/Header/Header.js
@@ -3,6 +3,6 @@ import "./styles.css";
 
 export const Header = () => (
   <div className="header-container">
-    <img src={Logo} width="15%" alt="company-logo" />
+    <img src={Logo} className="logo" width="15%" alt="company-logo" />
   </div>
 );
